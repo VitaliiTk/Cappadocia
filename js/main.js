9d9c05615703e463356bbfe852c4f4eac7bc4c92
Vitalii <tka4uk.gt41@gmail.com>
2021-04-08 17:32:34 +0600

    pc version finish

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$( function () {
+    $('.fairy-tail__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/fairy-tail/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/fairy-tail/arrow-right.svg" alt=""></button>',
+        fade: true,
+        autoplay: true,
+    })
+
+    $('.trip__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/fairy-tail/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/fairy-tail/arrow-right.svg" alt=""></button>',
+        fade: true,
+        autoplay: true,
+    })
+})
